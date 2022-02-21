661695ec20333d62d8f91671af844a8e29495110
tuong.lau16072002@hcmut.edu.vn <tuong.lau16072002@hcmut.edu.vn>
2022-02-22 06:47:36 +0700

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import Login from './components/Login/Login'
+import Main from './components/Main/Main'
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  
+  const [login, setLogin] = useState('false');
+  const [memberList, setMemberList] = useState([])
+  const [username, setUsername] = useState("");
+  
+ 
+
+  return (
+    <div className="App">
+      {login == true ? (
+        <Main
+          socket={socket}
+          memberList={memberList}
+          setMemberList={setMemberList}
+          username={username}
+        />
+      ) : (
+        <Login
+          memberList={memberList}
+          setLogin={setLogin}
+          socket={socket}
+          setMemberList={setMemberList}
+          setUsername={setUsername}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App
